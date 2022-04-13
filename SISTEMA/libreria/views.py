c5692d303d4c0874d8e561b31fae0a2b8d0c81bf
kikevis <villasanchezg22@gmail.com>
2022-04-13 11:38:17 -0500

    actualizacion

--- SISTEMA/libreria/views.py
+++ SISTEMA/libreria/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("<h1>Hola Mundo - Hello Wordl</h1>")
+    return HttpResponse("<h1>Hola Mundo - Hello World</h1>")
