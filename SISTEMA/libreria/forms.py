f033d95f74f4b4ec2583a3aec7e38e4c82157c7f
kikevis <villasanchezg22@gmail.com>
2022-04-18 13:56:22 -0500

    actualizacion

--- SISTEMA/libreria/forms.py
+++ SISTEMA/libreria/forms.py
@@ -4,4 +4,4 @@ from .models import Libro
 class LibroForm(forms.ModelForm):
     class Meta:
         model = Libro
-        field = '__all__'
+        fields = '__all__'
--- SISTEMA/libreria/urls.py
+++ SISTEMA/libreria/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('libros', views.libros, name='libros'),
     path('libros/crear', views.crear, name='crear'),
     path('libros/editar', views.editar, name='editar'),
+    path('libros/eliminar/<int:id>', views.eliminar, name='eliminar'),
+    path('libros/editar/<int:id>', views.editar, name='editar'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
